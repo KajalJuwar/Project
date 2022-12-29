f86b06fe267b46cd102a151cda0810716585c8e9
Kajal <kajaljuwar@gmail.com>
2022-12-29 10:50:38 +0530

    First commit

---
+++ src/com/example/Test.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class Test {
+	
+public static void main(String[] args) {
+		
+		ArrayList<String> al= new ArrayList<String>();
+		al.add("kajal");
+		al.add("nisha");
+		al.add("rupa");
+		System.out.println(al);
+		
+	
+		
+		
+	}
+
+}
